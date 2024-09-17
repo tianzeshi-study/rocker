86aa3fec1af2460ac6b92970e5f023a790bb64b3
luster <luster@flowersky.love>
2024-09-18 00:42:18 +0800

    pull from cn hub

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use shiplift::{Docker, PullOptions};
 use clap::{Parser, Subcommand};
+use futures::StreamExt;
+// use std::env;
 
 mod container;
 
@@ -51,6 +54,13 @@ enum DockerCommand {
         all: bool,
     },
 
+/// List images
+    Images {
+        /// Show all images (default hides intermediate images)                                   
+        #[arg(short, long)]
+        all: bool,
+    },
+
     /// Remove one or more containers
     Rm {
         /// Force the removal of a running container
@@ -60,7 +70,75 @@ enum DockerCommand {
         /// Container ID or name
         container: String,
     },
+
+    /// Pull an image or a repository from a registry
+    Pull {
+        /// The name of the image to pull, with optional TAG or DIGEST
+        #[arg(value_name = "NAME[:TAG|@DIGEST]")]
+        name: String,
+
+        /// Download all tagged images in the repository
+        #[arg(short = 'a', long = "all-tags")]
+        all_tags: bool,
+
+        /// Skip image verification (default true)
+        #[arg(long = "disable-content-trust", default_value = "true")]
+        disable_content_trust: bool,
+
+        /// Set platform if server is multi-platform capable
+        #[arg(long = "platform")]
+        platform: Option<String>,
+
+        /// Suppress verbose output
+        #[arg(short = 'q', long = "quiet")]
+        quiet: bool,
+    },
 }
+
+
+async fn images() {
+    let docker = Docker::new();
+    println!("docker images in stock");
+
+    let result = docker.images().list(&Default::default()).await;
+
+    match result {
+        Ok(images) => {
+            for i in images {
+                println!(
+                    "{} {} {:?}",
+                    i.id,
+                    i.created,
+                    i.repo_tags.unwrap_or_else(|| vec!["none".into()])
+                );
+            }
+        }
+        Err(e) => eprintln!("Error: {}", e),
+    }
+}
+
+
+async fn pull(name: String) {
+    // env_logger::init();
+    let docker = Docker::new();
+    // let img = env::args().nth(1).expect("You need to specify an image name");
+    let hub = "hub.aiursoft.cn/".to_string();
+    let img = hub+&name;
+
+    let mut stream = docker
+        .images()
+        .pull(&PullOptions::builder().image(img).build());
+
+    while let Some(pull_result) = stream.next().await {
+        match pull_result {
+            Ok(output) => println!("{:?}", output),
+            Err(e) => eprintln!("Error: {}", e),
+        }
+    }
+}
+
+
+
 
 #[tokio::main]
 async fn main() {
@@ -104,6 +182,15 @@ async fn main() {
             }
         }
         
+        DockerCommand::Images { all } => {
+            if *all {
+                println!("Listing all images");
+            } else {
+                images().await;
+            }
+        }
+
+        
         // DockerCommand::container { all } => {
             // if *all {
                 // println!("Listing all containers");
@@ -119,5 +206,28 @@ async fn main() {
                 println!("Forcing container removal");
             }
         }
-    }
+
+DockerCommand::Pull {
+            name,
+            all_tags,
+            disable_content_trust,
+            platform,
+            quiet,
+        } => {
+            println!("Pulling image: {}", name);
+            pull(name.to_string()).await;
+            if *all_tags {
+                println!("Downloading all tags...");
+            }
+            if !*disable_content_trust {
+                println!("Image verification enabled...");
+            }
+            if let Some(platform) = platform {
+                println!("Platform: {}", platform);
+            }
+            if *quiet {
+                println!("Running in quiet mode...");
+            }
+        }
+}
 }
